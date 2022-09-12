9bb4553f33575e91ab32649782ee5f8f736c8af8
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2022-09-12 12:48:11 +0200

    Created mouseevents

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ const app = Vue.createApp({
             showBooks: true,
             title: "The Final Empire",
             author: "Brandon Sanderson",
-            age: 46
+            age: 46,
+            x: 0,
+            y: 0
         }
     },
 
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+
+        handleEvent(event, data){
+            console.log(event)
+            if(data){
+                console.log(data)
+            }
+        },
+
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 });
